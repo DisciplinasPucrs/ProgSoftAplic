180aaa4e7b4c3c0fd98418752396a99e302e725b
Júlio Pereira Machado <julioapm@gmail.com>
2025-03-11 01:11:01 +0000

    Atualização das classes de domínio básicas

--- revisaoPadroes/src/Categoria.java
+++ revisaoPadroes/src/Categoria.java
@@ -2,5 +2,29 @@ public class Categoria {
     private int id;
     private String nome;
     private int desconto;
-    
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
 }
