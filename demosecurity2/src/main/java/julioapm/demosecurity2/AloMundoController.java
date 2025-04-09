07b8001f96f2d6838719bbfb59d5dacf94232c18
Júlio Machado <julioapm@gmail.com>
2025-04-09 15:54:21 -0300

    atualizacao demosecurity2

--- demosecurity2/src/main/java/julioapm/demosecurity2/AloMundoController.java
+++ demosecurity2/src/main/java/julioapm/demosecurity2/AloMundoController.java
@@ -1,13 +1,15 @@
 package julioapm.demosecurity2;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AloMundoController {
     @GetMapping("/alo")
-    public String alo() {
-        return "Alô, Mundo!";
+    public String alo(@AuthenticationPrincipal UserDetails userDetails) {
+        return "Alô, " + userDetails.getUsername() + "!";
     }
 
     @GetMapping("/ola")
