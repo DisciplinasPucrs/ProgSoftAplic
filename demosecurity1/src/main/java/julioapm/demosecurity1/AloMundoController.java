ebfebfb82d8ce72c5d57ce4df65b1dae2955337a
Júlio Machado <julioapm@gmail.com>
2025-04-09 14:40:58 -0300

    demosecurity1

---
+++ demosecurity1/src/main/java/julioapm/demosecurity1/AloMundoController.java
@@ -0,0 +1,12 @@
+package julioapm.demosecurity1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AloMundoController {
+    @GetMapping("/alo")
+    public String alo() {
+        return "Alô, Mundo!";
+    }
+}
---
+++ demosecurity1/src/test/java/julioapm/demosecurity1/Demosecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package julioapm.demosecurity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
