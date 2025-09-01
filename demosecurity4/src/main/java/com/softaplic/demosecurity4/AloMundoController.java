68407a0100833e69a7a3681f98f9230016086eec
Júlio Machado <julioapm@gmail.com>
2025-09-01 15:45:22 -0300

    atualizacao controller

--- demosecurity4/src/main/java/com/softaplic/demosecurity4/AloMundoController.java
+++ demosecurity4/src/main/java/com/softaplic/demosecurity4/AloMundoController.java
@@ -1,7 +1,6 @@
 package com.softaplic.demosecurity4;
 
 import java.security.Principal;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
