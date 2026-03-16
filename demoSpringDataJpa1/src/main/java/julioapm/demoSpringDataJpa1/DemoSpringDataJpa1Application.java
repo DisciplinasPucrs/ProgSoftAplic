dccc23e972b9ad96b5b5772323e8e85dfb311405
Júlio Machado <julioapm@gmail.com>
2026-03-16 14:49:58 -0300

    atualização pom

--- demoSpringDataJpa1/src/main/java/julioapm/demoSpringDataJpa1/DemoSpringDataJpa1Application.java
+++ demoSpringDataJpa1/src/main/java/julioapm/demoSpringDataJpa1/DemoSpringDataJpa1Application.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
